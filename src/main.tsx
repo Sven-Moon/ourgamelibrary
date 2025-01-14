a19e139080041b00697a64c5666065375fd794f0
Svenpire <svenpire@gmail.com>
2025-01-14 12:57:33 -0700

    merge from main

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,7 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </StrictMode>,
 )
